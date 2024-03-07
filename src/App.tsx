c8d1027823f074654719532741e1ab149f7b3601
Gajendra sahu <82883877+Gajmain2020@users.noreply.github.com>
2024-03-07 17:46:20 +0530

    bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar";
 import NotFound from "./components/NotFound/NotFound";
 import Footer from "./components/Footer";
